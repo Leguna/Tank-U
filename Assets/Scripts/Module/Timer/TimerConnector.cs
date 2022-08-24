bddcc4e72989387343397eb182e047e62fde6296
Ahmad Tuflihun <ahmadtuflihunxd@gmail.com>
2022-08-24 11:34:25 +0800

    TU-33 Add Game Controller Editor Button for start, pause, resume game, and add timer

---
+++ Assets/Scripts/Module/Timer/TimerConnector.cs
@@ -0,0 +1,25 @@
+﻿using Agate.MVC.Base;
+
+namespace TankU.Module.Timer
+{
+    public class TimerConnector : BaseConnector
+    {
+        private TimerController _controller;
+
+        protected override void Connect()
+        {
+            // TODO @Vivian and Mark: Need Subscribe to TimerMessage On Start, On Pause, On Resume
+            // Subscribe<>(_controller.OnStartGame());
+            // Subscribe<>(_controller.OnGamePause());
+            // Subscribe<>(_controller.OnGameResume());
+        }
+
+        protected override void Disconnect()
+        {
+            // TODO @Vivian and Mark: Need Subscribe to TimerMessage On Start, On Pause, On Resume
+            // Unsubscribe<>(_controller.OnStartGame());
+            // Unsubscribe<>(_controller.OnGamePause());
+            // Unsubscribe<>(_controller.OnGameResume());
+        }
+    }
+}
