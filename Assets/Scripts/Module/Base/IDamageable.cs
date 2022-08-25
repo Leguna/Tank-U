2798124d484452d5fdef11c01ba4cd41e548b70d
Ahmad Tuflihun <ahmadtuflihunxd@gmail.com>
2022-08-25 12:01:34 +0800

    TU-26-bullet add damageable interface

---
+++ Assets/Scripts/Module/Base/IDamageable.cs
@@ -0,0 +1,19 @@
+﻿namespace TankU.Module.Base
+{
+    public interface IDamageableModel
+    {
+        int Health { get; }
+
+        void TakeDamage(int damage);
+    }
+
+    public interface IDoingDamageModel
+    {
+        int Damage { get; }
+    }
+
+    public interface IDamageableView
+    {
+        void TakeDamage(IDoingDamageModel damageModel);
+    }
+}
