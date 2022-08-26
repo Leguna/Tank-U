d09edd9eac190059e285478188a00adceabc9a48
Ahmad Tuflihun <ahmadtuflihunxd@gmail.com>
2022-08-26 08:28:08 +0800

    fix error

--- Assets/Scripts/Gameplay/GameplayView.cs
+++ Assets/Scripts/Gameplay/GameplayView.cs
@@ -3,8 +3,6 @@ using TankU.Module.Bullet;
 using TankU.Module.ColourPicker;
 using TankU.Module.Timer;
 using TankU.PowerUp;
-using TankU.Gameplay;
-using UnityEngine;
 
 namespace TankU.Gameplay
 {
@@ -12,8 +10,10 @@ namespace TankU.Gameplay
     {
         public TimerView TimerView;
         public ColorPickerView ColorPickerView;
+
         // TODO @Leguna: Remove This after implement bullet pool
         public BulletView BulletView;
+        public PlayerView PlayerView;
         public PowerUpPoolerView powerUpPooler;
     }
 }
