b82f41c331d9f465e5d9b6f09ac77519797da4ff
Mark C <deathfated7@gmail.com>
2022-09-01 14:27:39 +0800

    bomb explosion timing fixed

--- Assets/Scripts/Module/Bomb/BombModel.cs
+++ Assets/Scripts/Module/Bomb/BombModel.cs
@@ -20,7 +20,7 @@ namespace TankU.Module.Bomb
             Damage = 1;
             Health = 0;
             SpawnTimer = 4f;
-            Duration = 8f;
+            Duration = 5.5f;
         }
 
         public void SpawnBomb(Vector3 spawnPosition, int damage, int health)
